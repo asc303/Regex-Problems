32252fee8be2e60c2a7b7f1bb60f2c68ed7a6777
asc303 <chauhansam94@gmail.com>
2021-11-30 22:46:48 +0530

    [ashutosh] add:Registration

---
+++ REGEXUserRegistration/REGEXUserRegistration/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace REGEXUserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to REGEX User Registration");
+            UserRegistration validation = new UserRegistration();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to the User Registration validation program");
+                Console.WriteLine("Enter the validation to Execute : \n 1. Candidate First Name \n 2. Candidate Last Name \n 3. Candidate Email \n 4. Mobile Number \n 5. Password Rule=1 \n 6. Password Rule=2 \n 7. Password Rule=3 \n 8. Password Rule=4 \n 9. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine(validation.ValidateFirstName("Ashutosh"));
+                        break;
+                    case 2:
+                        Console.WriteLine(validation.ValidateLastName("Singh"));
+                        break;
+                    case 3:
+                        Console.WriteLine(validation.ValidateEmail("chauhansam94@gmail.com"));
+                        break;
+                    case 4:
+                        Console.WriteLine(validation.ValidateMobile("91 9696896592"));
+                        break;
+                    case 5:
+                        Console.WriteLine(validation.ValidatePassword1("Ashu"));
+                        break;
+                    case 6:
+                        Console.WriteLine(validation.ValidatePassword2("Ashu1"));
+                        break;
+                    case 7:
+                        Console.WriteLine(validation.ValidatePassword3("Ashu2"));
+                        break;
+                    case 8:
+                        Console.WriteLine(validation.ValidatePassword4("Ashu3"));
+                        break;
+                    case 9:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ REGEXUserRegistration/REGEXUserRegistration/UserRegistration.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace REGEXUserRegistration
+{
+    public class UserRegistration
+    {
+        public static string REGEX_FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public static string REGEX_LAST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public static string REGEX_EMAIL = "^[a-z]{3,}[_+-.]{0,1}[a-z]{3,}[@][a-z]{2,}[.][a-z]{2}[.][a-z]{2}";
+        public static string REGEX_MOBILE = "^[1,9]{2} [6-9]{1}[0-9]{9}";
+        public static string REGEX_PASSWORD1 = "^[a-z]{8}";
+        public static string REGEX_PASSWORD2 = "^[A-Z]{1}[a-z]{7}$";
+        public static string REGEX_PASSWORD3 = "^[A-Z]{1}[a-z]{7}[0-9]{1}$";
+        public static string REGEX_PassWord4 = "^(?=.*[A-Z]).{8,}$";
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, REGEX_FIRST_NAME);
+        }
+        public bool ValidateLastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, REGEX_LAST_NAME);
+        }
+        public bool ValidateEmail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_EMAIL);
+        }
+        public bool ValidateMobile(string mobile)
+        {
+            return Regex.IsMatch(mobile, REGEX_MOBILE);
+        }
+        public bool ValidatePassword1(string password1)
+        {
+            return Regex.IsMatch(password1, REGEX_PASSWORD1);
+        }
+        public bool ValidatePassword2(string Password2)
+        {
+            return Regex.IsMatch(Password2, REGEX_PASSWORD2);
+        }
+        public bool ValidatePassword3(string password3)
+        {
+            return Regex.IsMatch(password3, REGEX_PASSWORD3);
+        }
+        public bool ValidatePassword4(string Password4)
+        {
+            return Regex.IsMatch(Password4, REGEX_PassWord4);
+        }
+
+    }
+}
