a88f25d23c7d90fdf8b31c5616429ef136638463
asc303 <chauhansam94@gmail.com>
2021-11-30 22:53:10 +0530

    [ashutosh] add:FirstName

--- REGEXUserRegistration/REGEXUserRegistration/Program.cs
+++ REGEXUserRegistration/REGEXUserRegistration/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace REGEXUserRegistration
 {
@@ -12,7 +13,7 @@ namespace REGEXUserRegistration
             while (flag)
             {
                 Console.WriteLine("Welcome to the User Registration validation program");
-                Console.WriteLine("Enter the validation to Execute : \n 1. Candidate First Name \n 2. Candidate Last Name \n 3. Candidate Email \n 4. Mobile Number \n 5. Password Rule=1 \n 6. Password Rule=2 \n 7. Password Rule=3 \n 8. Password Rule=4 \n 9. Exit");
+                Console.WriteLine("Enter the validation to Execute : \n 1. Candidate First Name \n 2. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -20,27 +21,6 @@ namespace REGEXUserRegistration
                         Console.WriteLine(validation.ValidateFirstName("Ashutosh"));
                         break;
                     case 2:
-                        Console.WriteLine(validation.ValidateLastName("Singh"));
-                        break;
-                    case 3:
-                        Console.WriteLine(validation.ValidateEmail("chauhansam94@gmail.com"));
-                        break;
-                    case 4:
-                        Console.WriteLine(validation.ValidateMobile("91 9696896592"));
-                        break;
-                    case 5:
-                        Console.WriteLine(validation.ValidatePassword1("Ashu"));
-                        break;
-                    case 6:
-                        Console.WriteLine(validation.ValidatePassword2("Ashu1"));
-                        break;
-                    case 7:
-                        Console.WriteLine(validation.ValidatePassword3("Ashu2"));
-                        break;
-                    case 8:
-                        Console.WriteLine(validation.ValidatePassword4("Ashu3"));
-                        break;
-                    case 9:
                         flag = false;
                         break;
                 }
